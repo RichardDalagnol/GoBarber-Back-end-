40694f81b904462f73b48133adeba8a01228a09d
Richard <richardbim123@gmail.com>
2019-09-14 19:02:37 -0300

    Crud

---
+++ src/AuthConfig.js
@@ -0,0 +1,4 @@
+export default {
+  expiresIn: "7d",
+  secret: "40f54123fe47a5a5ecde551f2de1965c",
+};
---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import * as yup from "yup";
+import Auth from "../../AuthConfig";
+import User from "../models/User";
+
+class SessionController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup
+        .string()
+        .email()
+        .required(),
+      password: yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(402).json({ error: "Validation fails" });
+    }
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: "User does not exists " });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: "Password does not match" });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, Auth.secret, {
+        expiresIn: Auth.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
